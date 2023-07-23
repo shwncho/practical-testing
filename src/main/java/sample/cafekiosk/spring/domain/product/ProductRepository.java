6772cbd55c34b3f92d5f0e5d5113544d67bb70d8
simple <shwncho4@gmail.com>
2023-07-24 00:15:29 +0900

    test: business layer test 1

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllBySellingTypeIn(List<ProductSellingType> sellingTypes);
+
+    List<Product> findAllByProductNumberIn(List<String> productNumbers);
 }
--- src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -59,4 +59,44 @@ class ProductRepositoryTest {
                 );
     }
 
+    @Test
+    @DisplayName("상품번호 리스트로 상품들을 조회한다.")
+    void findAllByProductNumberIn() throws Exception {
+
+        //given
+        Product product = Product.builder()
+                .productNumber("001")
+                .type(ProductType.HANDMADE)
+                .sellingType(ProductSellingType.SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(ProductType.HANDMADE)
+                .sellingType(ProductSellingType.HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+
+        Product product3 = Product.builder()
+                .productNumber("003")
+                .type(ProductType.HANDMADE)
+                .sellingType(ProductSellingType.STOP_SELLING)
+                .name("팥빙수")
+                .price(7000)
+                .build();
+        productRepository.saveAll(List.of(product,product2,product3));
+        //when
+        List<Product> products = productRepository.findAllByProductNumberIn(List.of("001","002"));
+        //then
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "sellingType")
+                .containsExactlyInAnyOrder(
+                        tuple("001","아메리카노", ProductSellingType.SELLING),
+                        tuple("002","카페라떼", ProductSellingType.HOLD)
+                );
+    }
+
 }
