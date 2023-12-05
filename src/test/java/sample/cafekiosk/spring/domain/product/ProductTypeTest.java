b6b699d1a8850061c64d7b39b0f2d490fa30122c
simple <shwncho4@gmail.com>
2023-12-05 18:36:28 +0900

    test: parameterizedtest

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -2,9 +2,14 @@ package sample.cafekiosk.spring.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTypeTest {
 
@@ -30,6 +35,34 @@ class ProductTypeTest {
         boolean result = ProductType.containsStockType(givenType);
         // then
         assertThat(result).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"HANDMADE,false","BOTTLE,true","BAKERY,true"})
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    void containsStockType4(ProductType productType, boolean expected) throws Exception{
+        //when
+        boolean result = ProductType.containsStockType(productType);
+        //then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideProductTypesForCheckingStockType")
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    void containsStockType5(ProductType productType, boolean expected) throws Exception{
+        //when
+        boolean result = ProductType.containsStockType(productType);
+        //then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideProductTypesForCheckingStockType(){
+        return Stream.of(
+                Arguments.of(ProductType.HANDMADE, false),
+                Arguments.of(ProductType.BOTTLE, true),
+                Arguments.of(ProductType.BAKERY, true)
+        );
     }
 
 }
